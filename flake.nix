d62d03d59e6128730b688e9269f14bcefb0b8566
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-03-08 08:10:21 -0300

    build(nix): update `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
+# Based on :: https://github.com/the-nix-way/dev-templates
+
 {
-  description = "A Nix-flake-based Elixir development environment";
+  description = "uCharge by @gersonfaneto";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
@@ -12,65 +14,31 @@
     in
     {
       overlays.default = final: prev: rec {
-        # documentation
-        # https://nixos.org/manual/nixpkgs/stable/#sec-beam
-
-        # ==== ERLANG ====
-
-        # use whatever version is currently defined in nixpkgs
-        # erlang = pkgs.beam.interpreters.erlang;
-
-        # use latest version of Erlang 27
         erlang = final.beam.interpreters.erlang_27;
-
-        # specify exact version of Erlang OTP
-        # erlang = pkgs.beam.interpreters.erlang.override {
-        #   version = "26.2.2";
-        #   sha256 = "sha256-7S+mC4pDcbXyhW2r5y8+VcX9JQXq5iEUJZiFmgVMPZ0=";
-        # }
-
-        # ==== BEAM packages ====
-
-        # all BEAM packages will be compile with your preferred erlang version
         pkgs-beam = final.beam.packagesWith erlang;
-
-        # ==== Elixir ====
-
-        # use whatever version is currently defined in nixpkgs
-        # elixir = pkgs-beam.elixir;
-
-        # use latest version of Elixir 1.17
         elixir = pkgs-beam.elixir_1_17;
-
-        # specify exact version of Elixir
-        # elixir = pkgs-beam.elixir.override {
-        #   version = "1.17.1";
-        #   sha256 = "sha256-a7A+426uuo3bUjggkglY1lqHmSbZNpjPaFpQUXYtW9k=";
-        # };
       };
 
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            # use the Elixr/OTP versions defined above; will also install OTP, mix, hex, rebar3
+            # Elixir
             elixir
             elixir-ls
 
-            # mix needs it for downloading dependencies
+            # Mix
             git
 
-            # probably needed for your Phoenix assets
+            # Phoenix
             nodejs_20
           ]
           ++
-          # Linux only
           pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
-            # gigalixir
+            # gigalixir # Somehow this is broken...
             inotify-tools
             libnotify
           ])
           ++
-          # macOS only
           pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
             terminal-notifier
             darwin.apple_sdk.frameworks.CoreFoundation
