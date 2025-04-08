c8fac45321cc62014159787567d3ebd4fb3732fd
vini464 <vini.dev2003@gmail.com>
2025-04-08 00:13:48 -0300

    refact(core): change code block for `get_invalid_body` function.

--- core/server/src/main.rs
+++ core/server/src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, TcpStream};
 
 use http::http::HttpMethod;
 use http::middleware::logger::LoggerMiddleware;
@@ -8,9 +8,9 @@ use http::response::Response;
 use http::server::{FutureResponse, ServerBuilder};
 
 use models::station::Station;
- 
 
 static mut STATIONS: Vec<Station> = Vec::new();
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,34 +18,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     ServerBuilder::new()
         .bind(addr)
         .route("/health", HttpMethod::GET, health_handler)
-        .route("/confirm", HttpMethod::GET, confirm_handler)
+        .route("/confirm", HttpMethod::POST, confirm_handler)
         .route("/connect", HttpMethod::POST, connect_handler)
         .accept(LoggerMiddleware)
         .build()?
         .run()
         .await?;
 
-       //.route("/login", HttpMethod::POST, handler)
-       //.route("/sign_in", HttpMethod::POST, handler)
-       //.route("/charge", HttpMethod::POST, handler)
-       //.route("/arrived", HttpMethod::POST, handler)
-       //.route("/charging", HttpMethod::POST, handler)
-       //.route("/charged", HttpMethod::POST, handler)
+    //.route("/login", HttpMethod::POST, handler)
+    //.route("/sign_in", HttpMethod::POST, handler)
+    //.route("/charge", HttpMethod::POST, handler)
+    //.route("/arrived", HttpMethod::POST, handler)
+    //.route("/charging", HttpMethod::POST, handler)
+    //.route("/charged", HttpMethod::POST, handler)
 
-       //.route("/update", HttpMethod::POST, handler)
+        //.route("/update", HttpMethod::POST, update_handler)
     Ok(())
 }
 
 fn connect_handler(request: Request) -> FutureResponse<'static> {
     match request.body {
         Some(body) => {
-            
-            let end = body.find("}").unwrap() + 1;
-            let connect_body: Result<ConnectBody, serde_json::Error> = serde_json::from_str(&body[..end]);
+            let end = body.find("\0").unwrap();
+            let connect_body: Result<ConnectBody, serde_json::Error> =
+                serde_json::from_str(&body[..end]);
             match connect_body {
                 Ok(body) => {
                     unsafe {
-                        STATIONS.push(Station::new(body.position, body.power, body.price, body.name));
+                        STATIONS.push(Station::new(
+                            body.position,
+                            body.power,
+                            body.price,
+                            body.name,
+                        ));
                     }
                     let response = Response {
                         version: "HTTP/1.1".to_string(),
@@ -55,36 +60,20 @@ fn connect_handler(request: Request) -> FutureResponse<'static> {
                         body: None,
                     };
                     unsafe {
-                    for station in STATIONS.iter() {
+                        for station in STATIONS.iter() {
                             eprintln!("{}", station);
-                    }
+                        }
                     }
                     eprintln!("[info] - station added");
-                    return Box::pin(async move { Ok(response) })
-                },
-                Err(e) => {
-                    eprintln!("{e}");
-                    let response = Response {
-                        version: "HTTP/1.1".to_string(),
-                        status_code: 400,
-                        status_text: "Invalid Body".to_string(),
-                        headers: HashMap::new(),
-                        body: None,
-                    };
-                    return Box::pin(async move { Ok(response) })
-                } 
+                    return Box::pin(async move { Ok(response) });
+                }
+                Err(_) => {
+                    get_invalid_body()
+                }
             }
-        },
+        }
         None => {
-            let response = Response {
-                version: "HTTP/1.1".to_string(),
-                status_code: 404,
-                status_text: "Not Found".to_string(),
-                headers: HashMap::new(),
-                body: None,
-            };
-            return Box::pin(async move { Ok(response) })
-
+            get_invalid_body()
         }
     }
 }
@@ -92,24 +81,12 @@ fn connect_handler(request: Request) -> FutureResponse<'static> {
 fn confirm_handler(request: Request) -> FutureResponse<'static> {
     match request.body {
         Some(body) => {
-
-            let end = body.find("}").unwrap() + 1;
-            let confirm_body: ConfirmBody = serde_json::from_str(&body[..end]).unwrap_or_else(|_| {
-                ConfirmBody {token: String::from("None"), plate: String::from("None")}
-            });
-            match confirm_body.token.as_str() {
-                "None" => {
-                    let response = Response {
-                        version: "HTTP/1.1".to_string(),
-                        status_code: 400,
-                        status_text: "Invalid body".to_string(),
-                        headers: HashMap::new(),
-                        body: None,
-                    };
-                    return Box::pin(async move { Ok(response) })
-                }
-                token => {
-                    let html = format!("<html><body><h1>Hello Person {token}!</h1></body></html>");
+            let end = body.find("\0").unwrap();
+            let confirm_body: Result<ConfirmBody, serde_json::Error> =
+                serde_json::from_str(&body[..end]);
+            match confirm_body {
+                Ok(body) => {
+                    let html = format!("<html><body><h1>Hello Person {}!</h1></body></html>", body.token);
 
                     let response = Response {
                         version: "HTTP/1.1".to_string(),
@@ -122,22 +99,18 @@ fn confirm_handler(request: Request) -> FutureResponse<'static> {
                         },
                         body: Some(html.to_string()),
                     };
-                    return Box::pin(async move { Ok(response) })
+                    return Box::pin(async move { Ok(response) });
+
+            }
+                Err(_) => {
+                    get_invalid_body()
                 }
             }
-        },
-        None => {
-            let response = Response {
-                version: "HTTP/1.1".to_string(),
-                status_code: 404,
-                status_text: "Not Found".to_string(),
-                headers: HashMap::new(),
-                body: None,
-            };
-            return Box::pin(async move { Ok(response) })
         }
-    };
-    
+        None => {
+            get_invalid_body()
+        }
+    }
 }
 
 fn health_handler(_: Request) -> FutureResponse<'static> {
@@ -158,3 +131,13 @@ fn health_handler(_: Request) -> FutureResponse<'static> {
     Box::pin(async move { Ok(response) })
 }
 
+fn get_invalid_body() -> FutureResponse<'static> {
+    let response = Response {
+        version: "HTTP/1.1".to_string(),
+        status_code: 404,
+        status_text: "Not Found".to_string(),
+        headers: HashMap::new(),
+        body: None,
+    };
+    return Box::pin(async move { Ok(response) });
+}
